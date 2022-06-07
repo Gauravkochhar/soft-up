cac5d43401dc462442fee4278b94adc220dcb3fa
rajenderrohila <rajenderrohilaa@gmail.com>
2022-06-07 23:58:48 +0530

    UI

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,13 +9,14 @@ import { SlickCarouselComponent } from 'ngx-slick-carousel';
 export class DashboardComponent implements OnInit {
   
   leftSlideConfig = {
-    slidesToShow:1, 
+    slidesToShow:2.8, 
     slidesToScroll:1,
     centerMode:true,
     vertical:true,
-    dots:true,
+    dots:false,
+    nav:false,
     infinite: false,
-    verticalSwiping: false,
+    verticalSwiping: true,
     adaptiveHeight: true,
     centerPadding: '0px'
     
@@ -28,14 +29,16 @@ export class DashboardComponent implements OnInit {
     dots:true,
     infinite: false,
     adaptiveHeight: true,
-    centerPadding: '20'
+    centerPadding: '10',
+    prevArrow: "<button type='button' class='slick-prev pull-left'><img width='30px' height='30px' src='assets/images/arrow-left.jpeg'></button>",
+    nextArrow: "<button type='button' class='slick-next pull-right'><img width='30px' height='30px' src='assets/images/arrow-right.jpeg'></button>",
   };
   
   slides = [
-    {img: "assets/images/agc6202.png"},
-    {img: "assets/images/agc6202.png"},
-    {img: "assets/images/agc6202.png"},
-    {img: "assets/images/agc6202.png"}
+    {heading:'City View', desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer luctus neque augue, bibendum auctor lectus blandit sit amet. Nullam neque risus, feugiat a ligula nec, rutrum imperdiet felis.'},
+    {heading:'Saran', desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer luctus neque augue, bibendum auctor lectus blandit sit amet. Nullam neque risus, feugiat a ligula nec, rutrum imperdiet felis.'},
+    {heading:'Back to the nast', desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer luctus neque augue, bibendum auctor lectus blandit sit amet. Nullam neque risus, feugiat a ligula nec, rutrum imperdiet felis.'},
+    {heading:'demo 1', desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer luctus neque augue, bibendum auctor lectus blandit sit amet. Nullam neque risus, feugiat a ligula nec, rutrum imperdiet felis.'}
   ];
   images = [
     'assets/logo/place.png',
